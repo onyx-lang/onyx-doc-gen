61881455fe3886e087fec8c6b75a3294e6902213
Brendan Hansen <brendan.f.hansen@gmail.com>
2023-03-30 13:15:38 -0500

    added: search functionality

--- src/parts/script.js
+++ src/parts/script.js
@@ -39,5 +39,50 @@ window.addEventListener("load", function () {
         let panel = elem.nextElementSibling;
         panel.style.maxHeight = panel.scrollHeight + "px";
     });
+
+    let fuse = new Fuse(window.PACKAGE_INDEX, {
+        keys: ['name', 'package_name']
+    });
+
+    let searchbar = document.querySelector("#package-search");
+    searchbar.addEventListener("focus", function(ev) {
+        document.querySelector(".search-results").style.display = "initial";
+    });
+    searchbar.addEventListener("unfocus", function(ev) {
+        document.querySelector(".search-results").style.display = "none";
+    });
+
+    searchbar.addEventListener("input", function(ev) {
+        let result_html = "";
+        let results = fuse.search(ev.target.value, { limit: 5 });
+        for (let res of results) {
+            let redirection = `/packages/${res.item.package}.html#${res.item.name}`;
+            let name = res.item.name;
+
+            result_html += `
+                <li data-redirect="${redirection}">
+                    <span class="search-result-badge">${res.item.type}</span>
+                    <span class="search-result-name">${name}</span>
+                    <span class="search-result-package">${res.item.package}</span>
+                </li>
+            `;
+        }
+
+        document.querySelector(".search-results").innerHTML = result_html;
+        document.querySelectorAll(".search-results li").forEach(e => {
+            e.addEventListener("click", (ev) => {
+                document.querySelector(".search-results").style.display = "none";
+                window.location.href = e.getAttribute("data-redirect");
+            });
+        });
+    });
+
+    searchbar.addEventListener("keydown", function (ev) {
+        if (ev.key == 'Enter') {
+            let li = document.querySelector(".search-results :first-child");
+
+            window.location.href = li.getAttribute("data-redirect");
+        }
+    });
 });
 
